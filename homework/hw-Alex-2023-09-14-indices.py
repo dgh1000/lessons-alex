be587f8b2dccd1fa7ab7975c37029100d7536620
Michael Mossey <michaelmossey@gmail.com>
2023-09-15 13:08:39 -0700

    new homework file

---
+++ homework/hw-Alex-2023-09-14-indices.py
@@ -0,0 +1,25 @@
+
+
+# Uncomment the following code. You'll use it in every problem
+# below.
+
+# s1 = "hello"
+
+# question #1. 
+
+# Now write code that prints every other letter in s1. Use
+# the 3-parameter form of range() to do this.
+
+# question #2.
+
+# Write code that prints the first and last letters in s1. You
+# won't use range() for this. You'll just subscript s1 (using
+# square brackets) to get the first and last letters.
+
+# question #3.
+
+# Uncomment the following line
+# s2 = "hellothere"
+
+# Now Write code that prints every third letter of the string.
+# Use range() to do this.
