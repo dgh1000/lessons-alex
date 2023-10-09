8f1bb420e7c01d3be251d83a339189633e1b21d0
Mike Mossey <michaelmossey@gmail.com>
2023-10-09 00:40:29 -0700

    new homework

---
+++ homework/turtle/turtle-0400-functions-A.py
@@ -0,0 +1,23 @@
+# Prequisites: basic familiarity with turtle graphics
+# including commands like forward, backward, left, right,
+# penup, pendown, speed, and color.
+
+# Topics Covered:
+# 1) Drawing letters in preparation for turning them into
+#    functions.
+
+# Question #1:
+#
+# Write a turtle graphics program that draws the letter A.
+
+# Question #2:
+#
+# Modify your program to draw the letter B also along with the
+# letter A. (You can draw them in any order you want.)
+
+# Question #3:
+#
+# Now modify the program so that when it draws the A or B, it
+# ends up at the same place facing the same direction it started.
+# (Hint: you will need to use penup and pendown.)
+
