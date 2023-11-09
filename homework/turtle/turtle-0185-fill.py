c940519c8daa265c5af02f377094212a6b82ba6b
Mike Mossey <michaelmossey@gmail.com>
2023-11-08 16:27:34 -0800

    fill

---
+++ homework/turtle/turtle-0185-fill.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+bgcolor("magenta")
+color("white")
+pensize(5)
+speed(0)
+pendown()
+begin_fill()
+forward(150)
+left(90)
+forward(150)
+left(90)
+forward(150)
+left(90)
+forward(150)
+end_fill()
+
+mainloop()
