6bcd1b4c0fc5f48f2f5256c072dd36764ff9419d
Mike Mossey <michaelmossey@gmail.com>
2024-11-10 22:38:43 -0800

    .gitignore, added homework

---
+++ homework/2024/homework-2024-11-09.py
@@ -0,0 +1,21 @@
+# Perse Coding Team Challenge: section "2020-21 R1 Review Mode"
+
+# Question 1:
+#
+# (this is based on question 2021 R1 Q2: Factory Hopperville)
+#
+# Let me show you some code to compare strings alphabetically:
+#
+# s1 = "Mike"
+# s2 = "Alex"
+# if s1 < s2:
+#     print(s1 + " is first alphabetically")
+# else:
+#     print(s2 + " is first alphabetically")
+
+# Q 1A. Uncomment this code, predict what it will do, then run it.
+#       (also type it yourself to get it under your fingers)
+# Q 1B. Modify s1 and s2 so the that the other branch of the if runs.
+# Q 1C. Now modify this program so it inputs two strings rather than
+#       using the hard-coded string literals you see here.
+
---
+++ homework/2024/homework-2024-11-10.py
@@ -0,0 +1,16 @@
+# homework-2024-11-10.py:
+#
+# based on PCTC 2023-24 R1 Review Mode, Y7-9 Junior
+#
+#
+# Q1: do Q1 again just to review basic concepts of input and strings
+
+# Q2: do Q2 again just to review basic concepts of input and math
+
+# Q3: do Q3 again 
+#
+# to review first
+#
+# Q3 A:
+#   write a program that inputs a string and prints it. Now write a program
+#   that inputs two ints and multiplies them and prints the result
