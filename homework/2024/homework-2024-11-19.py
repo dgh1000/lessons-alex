798d207b746054de588f0d6180dddc13d36db94d
Mike Mossey <michaelmossey@gmail.com>
2024-11-19 18:03:44 -0800

    added homework

---
+++ homework/2024/homework-2024-11-19.py
@@ -0,0 +1,29 @@
+# homework-2024-11-19.py:
+#
+# based on PCTC 2023-24 R1 Review Mode, Y7-9 Junior,
+#   continuing from homework-2024-11-10.py with Q4
+#
+# Q4: part A (based on Q4 from the above PCTC contest)
+#
+# Run these two programs and compare the output
+# Program 1:
+# print("-")
+# print("-")
+# Program 2:
+# print("-", end="")
+# print("-")
+#
+# Q4 Part B: see if this helps you complete Q4 from PCTC
+
+# Q4 Part C:
+# Here we are looking at ANOTHER way to complete PCTC Q4.
+# Run this program and try to understand it:
+# my_string = ""
+# my_string += "-"
+# print(my_string)
+# my_string += "-"
+# print(my_string)
+#
+# Q4 Part D: see if this helps you complete PCTC Q4 in 
+#   another way.
+
