502cc725077940e3a085e61c1f2b7efeb082929e
Mike Mossey <michaelmossey@gmail.com>
2024-11-26 04:24:34 -0800

    new homework for 11/27 lesson

---
+++ homework/2024/homework-2024-11-26.py
@@ -0,0 +1,24 @@
+# homework-2024-11-19.py:
+#
+# based on PCTC 2023-24 R1 Review Mode, Y7-9 Junior,
+#   continuing from homework-2024-11-10.py with Q5
+
+# Q5 A. I don't know how good you are with writing (English), Alex,
+#  but good practice for Q5 might be to write the directions in your
+#  own words. In particular, see if you can shorten the lengthy 
+#  problem description while still keeping the essence of it.
+
+# Q5 B. Then shorten your description even more until each sentence 
+# corresponds to one line of code. (Put each English sentence on its
+# own line.) For example, you could write the following in English
+# to describe one line of code:
+
+# Let me give some examples:
+
+# English: "Input the user's name."
+# Python: name = input("Enter your name: ")
+
+# English: "If they entered yes, do the following."
+# Python: if response == "yes":
+#             <the following goes here>
+
